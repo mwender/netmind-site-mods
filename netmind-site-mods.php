7d37add12108aa94624c052545ffc7a234bca93d
Michael Wender <michael@michaelwender.com>
2019-08-15 07:12:43 -0400

    First commit

---
+++ netmind-site-mods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Netmind Site Mods
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Miscellaneous site mods to the Netmind site. Instead of adding code to the site's theme functions.php, we're adding it here since our theme isn't under version control. Mods stored here include mods to , , etc.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     netmind-site-mods
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Netmind_Site_Mods
+ */
+
+// Your code starts here.
