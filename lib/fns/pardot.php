a1d38e25fa3a1d0e49347e6016f212b63abf4574
Michael Wender <michael@michaelwender.com>
2020-04-21 09:08:42 -0400

    Adding Pardot script to site footer

--- lib/fns/pardot.php
+++ lib/fns/pardot.php
@@ -17,3 +17,25 @@ function pardot_form_filter( $args, $record ){
   return $args;
 }
 add_filter( 'elementor_pro/forms/webhooks/request_args', __NAMESPACE__ . '\\pardot_form_filter', 10, 2 );
+
+function pardot_script(){
+?>
+<script type="text/javascript">
+/* Pardot Tracking */
+piAId = '541272';
+piCId = '12208';
+piHostname = 'pi.pardot.com';
+
+(function() {
+    function async_load(){
+        var s = document.createElement('script'); s.type = 'text/javascript';
+        s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+    }
+    if(window.attachEvent) { window.attachEvent('onload', async_load); }
+    else { window.addEventListener('load', async_load, false); }
+})();
+</script>
+<?php
+}
+add_action( 'wp_footer', __NAMESPACE__ . '\\pardot_script', 999 );
