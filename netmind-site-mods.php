2133310fe932e8a36d3883429f423433a2d9acb9
Michael Wender <michael@michaelwender.com>
2020-12-28 13:16:01 -0500

    Adding `uber_log()`

--- netmind-site-mods.php
+++ netmind-site-mods.php
@@ -24,3 +24,23 @@ require_once( 'lib/fns/knowledge-center.php' );
 require_once( 'lib/fns/pardot.php' );
 require_once( 'lib/fns/utilities.php' );
 require_once( 'lib/fns/woocommerce.php' );
+
+
+/**
+ * Enhanced logging.
+ *
+ * @param      string  $message  The log message
+ */
+if( ! function_exists( 'uber_log') ){
+  function uber_log( $message = null ){
+    static $counter = 1;
+
+    $bt = debug_backtrace();
+    $caller = array_shift( $bt );
+
+    if( 1 == $counter )
+      error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+    error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+    $counter++;
+  }
+}
