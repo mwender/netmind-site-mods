b57463279636bba19e540ba87b5d4caca6270a10
Michael Wender <michael@michaelwender.com>
2020-12-29 12:00:22 -0500

    Adding `filter` attribute to [netmind_related_posts]

--- lib/fns/related-posts.php
+++ lib/fns/related-posts.php
@@ -2,51 +2,18 @@
 
 namespace NetmindSiteMods\relatedposts;
 
-function related_posts( $atts ){
-  $args = shortcode_atts([
-    'numberposts' => 30,
-    'orderby'     => 'date',
-    'order'       => 'DESC',
-  ], $atts );
-
-  $alert = \netmind_get_alert([
-    'type' => 'info',
-    'title' => '`netmind_related_posts` shortcode',
-    'description' => 'The <code>netmind_related_posts</code> shortcode will be output here.'
-  ]);
-
-  $query_args = [
-    'numberposts' => $args['numberposts'],
-  ];
-  $posts = get_posts( $query_args );
-  if( $posts ){
-    $x = 0;
-    foreach ( $posts as $post ) {
-      $data[$x] = [
-        'title'     => $post->post_title,
-        'permalink' => get_the_permalink( $post->ID ),
-        'date'      => $post->post_date,
-        'thumbnail' => get_the_post_thumbnail_url( $post->ID, 'large' ),
-      ];
-      $taxonomies = ['knowledge_area','resource_type'];
-      foreach( $taxonomies as $taxonomy ){
-        $terms = wp_get_post_terms( $post->ID, $taxonomy );
-        if( $terms ){
-          $terms_array = [];
-          foreach( $terms as $term ){
-            $terms_array[] = $term->name;
-          }
-          $data[$x]['terms'][$taxonomy] = implode( ', ', $terms_array );
-        }
-      }
-      $x++;
-    } // foreach
-  }
-
-  wp_enqueue_style( 'relatedposts' );
-  wp_enqueue_script( 'relatedposts' );
-
+/**
+ * Gets the post html.
+ *
+ * @param      array   $data   The data
+ *
+ * @return     string  The post html.
+ */
+function get_post_html( $data = [] ){
   $template = netmind_get_template( 'list-item' );
+
+  if( empty( $data ) )
+    return '<p>NO DATA!</p>';
 
   $html[] = '<div class="related-posts">';
   foreach( $data as $item ){
@@ -76,5 +43,82 @@ function related_posts( $atts ){
   }
 
   return implode( '', $html );
+}
+
+/**
+ * Callback for the [netmind_related_posts /] shortcode.
+ *
+ * @param      array  $atts   {
+ *     Optional. An array of arguments.
+ *
+ *     @type int $numberposts Number of posts to display. Default 30.
+ *     @type str $orderby     The field to order the results by. Default 'date'.
+ *     @type str $order       ASC or DESC. Default DESC.
+ *     @type str $filter      Comma separated list of taxonomies used to filter
+ *                            the results. Used on a single post view, these
+ *                            will filter the results by taxonomy terms of the
+ *                            current post. Default: null.
+ * }
+ *
+ * @return     string  HTML for the Related Posts display.
+ */
+function related_posts( $atts ){
+  $args = shortcode_atts([
+    'numberposts' => 30,
+    'orderby'     => 'date',
+    'order'       => 'DESC',
+    'filter'      => null,
+  ], $atts );
+
+  $alert = \netmind_get_alert([
+    'type' => 'info',
+    'title' => '`netmind_related_posts` shortcode',
+    'description' => 'The <code>netmind_related_posts</code> shortcode will be output here.'
+  ]);
+
+  $query_args = [
+    'numberposts' => $args['numberposts'],
+  ];
+
+  if( ! is_null( $args['filter'] ) && is_single() ){
+    global $post;
+    $filters = ( stristr( $args['filter'], ',' ) )? explode( ',', $args['filter'] ) : [ $args['filter'] ];
+    foreach ($filters as $taxonomy ) {
+      $terms = get_the_terms( $post->ID, $taxonomy );
+      $term_ids = array_column( $terms, 'term_id' );
+      $query_args['tax_query'][] = [ 'taxonomy' => $taxonomy, 'terms' => implode(',', $term_ids ) ];
+    }
+  }
+
+  $posts = get_posts( $query_args );
+  if( $posts ){
+    $x = 0;
+    foreach ( $posts as $post ) {
+      $data[$x] = [
+        'title'     => $post->post_title,
+        'permalink' => get_the_permalink( $post->ID ),
+        'date'      => $post->post_date,
+        'thumbnail' => get_the_post_thumbnail_url( $post->ID, 'large' ),
+      ];
+      $taxonomies = ['knowledge_area','resource_type'];
+      foreach( $taxonomies as $taxonomy ){
+        $terms = get_the_terms( $post->ID, $taxonomy ); // wp_get_post_terms
+        if( $terms ){
+          $terms_array = [];
+          foreach( $terms as $term ){
+            $terms_array[] = $term->name;
+          }
+          $data[$x]['terms'][$taxonomy] = implode( ', ', $terms_array );
+        }
+      }
+      $x++;
+    } // foreach
+  }
+
+  wp_enqueue_style( 'relatedposts' );
+  wp_enqueue_script( 'relatedposts' );
+  $html = get_post_html( $data );
+
+  return $html;
 }
 add_shortcode( 'netmind_related_posts', __NAMESPACE__ . '\\related_posts' );
