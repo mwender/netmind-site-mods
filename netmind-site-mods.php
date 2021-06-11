4f6eb6c391c9ac803d8ac618f8fcaab53ae2302c
Javier Flaqué <jflaque@gmail.com>
2021-06-11 11:20:48 +0200

    1.3.5

--- netmind-site-mods.php
+++ netmind-site-mods.php
@@ -6,7 +6,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     netmind-site-mods
  * Domain Path:     /languages
- * Version:         1.3.3
+ * Version:         1.3.5
  * GitHub Plugin URI: mwender/netmind-site-mods
  * GitHub Plugin URI: https://github.com/mwender/netmind-site-mods
  *
