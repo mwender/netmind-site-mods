5dc64860f80684e926073dd36fbfb2054cd2946c
Michael Wender <michael@michaelwender.com>
2021-07-22 14:25:55 -0400

    Applying translation of `Title` only to `es_ES` locale

--- lib/fns/translations.php
+++ lib/fns/translations.php
@@ -3,12 +3,9 @@
 namespace NetmindSiteMods\translations;
 
 function translations( $translated, $original, $domain ){
-  if( ! 'es_ES' == NETMIND_LOCALE )
-    return;
-
   switch ( $original ) {
     case 'Title':
-      if( 'andalu_woo_courses' == $domain )
+      if( 'es_ES' == NETMIND_LOCALE && 'andalu_woo_courses' == $domain )
         $translated = 'Cargo';
       break;
 
